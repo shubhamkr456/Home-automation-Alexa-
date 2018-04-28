ac895a0a478019f68194e8cae276efebb752b959
shubhamkr456 <shubhamkr.456@gmail.com>
2018-04-29 00:56:08 +0530

    Add files via upload

---
+++ project/app.py
@@ -0,0 +1,60 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask, render_template,request, redirect, url_for
+import time
+import RPi.GPIO as GPIO
+
+app= Flask(__name__)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+#wsgi_app = app.wsgi_app
+
+led_pin=26
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.output(led_pin,GPIO.LOW)
+
+
+#apis
+@app.route('/', methods = ['POST','GET'])
+def index():
+    # variables for template page (templates/index.html)
+    author = "Shubham"
+    # if we make a post request on the webpage aka press button then do stuff
+    if request.method == 'POST':
+        # if we press the turn on button
+        if request.form['submit'] == 'Turn On': 
+            print ("TURN ON")
+            GPIO.output(led_pin,GPIO.HIGH)
+
+            # turn on LED on arduino
+            #stop automation
+            
+           
+        # if we press the turn off button
+        elif request.form['submit'] == 'Turn Off': 
+            print ('TURN OFF')
+            GPIO.output(led_pin,GPIO.LOW)
+        elif request.form['submit'] == 'manual':
+            f= open("abc.txt", "w+")
+            f.write("manual\n")
+            f.close()
+        elif request.form['submit'] == 'auto':
+            f= open("abc.txt", "w")
+            f.write("auto\n")
+            f.close()
+
+            
+           
+
+        else:
+            pass    
+    # read in analog value from photoresistor
+    
+    # the default page to display will be our template with our template variables
+    return render_template('index.html', author=author, value=0)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5040, debug=True)
